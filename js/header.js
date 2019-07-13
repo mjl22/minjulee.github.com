f63b66c8b5a650be436139e096d3a31fcb3c2db6
mjl22 <mjl22.27@gmail.com>
2019-07-13 17:37:38 -0400

    190711 Integrated 02

---
+++ js/header.js
@@ -0,0 +1,9 @@
+
+
+$(function(){
+  $(".navbar-toggle").click(function(){
+    /*//Animação com loop infinito (até clicar)
+    $("#d2").toggleClass("rotating");*/
+    $(".navbar-toggle").toggleClass("spinEffect");
+  });
+});
--- js/resume.js
+++ js/resume.js
@@ -15,6 +15,7 @@
     }
   });
 
+
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
     $('.navbar-collapse').collapse('hide');
@@ -23,6 +24,39 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#sideNav'
+  });
+
+})(jQuery); // End of use strict
+
+
+/////////////////////////////////
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-iframe[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-iframe').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#subNav'
   });
 
 })(jQuery); // End of use strict
