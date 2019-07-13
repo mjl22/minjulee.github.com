7b490159d37cf28e01ab0501c5790696bc4ed728
mjl22 <mjl22.27@gmail.com>
2019-07-13 17:45:46 -0400

    090711 php connected

--- prototype.php
+++ prototype.php
@@ -1,5 +1,5 @@
 <?php
 $content = file_get_contents('https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FIdrnB22zcR9R7mDqRX6rcYnS%2FUntitled%3Fnode-id%3D1%253A2%26viewport%3D138%252C272%252C0.10172070562839508%26scaling%3Dscale-down');
-$content = str_replace('</title>','</title><base href="High-fidelity prototypes" />', $content);
-$content = str_replace('</head>','<link rel="stylesheet" href="css/hello.css" /></head>', $content);
+$content = str_replace('</title>','</title><base href="https://www.figma.com" />', $content);
+$content = str_replace('</head>','<link rel="stylesheet" href="mjl22.github.io/css/hello.css" /></head>', $content);
 echo $content;
