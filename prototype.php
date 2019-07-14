633a2f061a23543d2d06749a3776e3bd07318a86
mjl22 <mjl22.27@gmail.com>
2019-07-13 20:39:54 -0400

    190711 php connection2

--- prototype.php
+++ prototype.php
@@ -1,5 +1,9 @@
+
 <?php
 $content = file_get_contents('https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FIdrnB22zcR9R7mDqRX6rcYnS%2FUntitled%3Fnode-id%3D1%253A2%26viewport%3D138%252C272%252C0.10172070562839508%26scaling%3Dscale-down');
-$content = str_replace('</title>','</title><base href="https://www.figma.com" />', $content);
-$content = str_replace('</head>','<link rel="stylesheet" href="mjl22.github.io/css/hello.css" /></head>', $content);
-echo $content;
+
+$changes_in_frame = '.header--header--3d0kA, .header--headerClear--14eAa, .header--headerDark--2JGe3 { display:none; }' . "\r\n" . '</style>';
+$frame_content = str_replace('</style>', $changes_in_frame, $frame_content);
+
+echo $frame_content;
+?>
