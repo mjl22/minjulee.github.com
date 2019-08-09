980bf789b02b890ba2223f223999850d3c5813ba
MINJU LEE <mjl22.27@gmail.com>
2019-08-10 03:05:25 +0900

    123
    123

--- README.md
+++
@@ -1,2 +0,0 @@
-# xxminjulee.github.com
-Portfolio
--- js/header.js
+++
@@ -1,9 +0,0 @@
-
-
-$(function(){
-  $(".navbar-toggle").click(function(){
-    /*//Animação com loop infinito (até clicar)
-    $("#d2").toggleClass("rotating");*/
-    $(".navbar-toggle").toggleClass("spinEffect");
-  });
-});
